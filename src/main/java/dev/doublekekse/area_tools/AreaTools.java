6da5d6aa874e1a755b26a5d2811a09d803b49c60
Tomate 06 13 <69756782+Tomate0613@users.noreply.github.com>
2025-02-11 14:47:47 +0100

    Add spawnpoint setter to OP tab

--- src/main/java/dev/doublekekse/area_tools/AreaTools.java
+++ src/main/java/dev/doublekekse/area_tools/AreaTools.java
@@ -1,16 +1,12 @@
 package dev.doublekekse.area_tools;
 
-import dev.doublekekse.area_lib.AreaLib;
 import dev.doublekekse.area_tools.command.AreaTrackCommand;
-import dev.doublekekse.area_tools.data.AreaToolsSavedData;
 import dev.doublekekse.area_tools.registry.AreaComponents;
 import dev.doublekekse.area_tools.registry.AreaItems;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.SharedConstants;
-import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.entity.player.Player;
@@ -27,6 +23,7 @@ public class AreaTools implements ModInitializer {
 
         ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.OP_BLOCKS).register(content -> {
             content.accept(AreaItems.AREA_CREATOR);
+            content.accept(AreaItems.SPAWNPOINT_SETTER);
         });
 
         CommandRegistrationCallback.EVENT.register(
